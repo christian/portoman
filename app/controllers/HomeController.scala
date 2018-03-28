de5efa9701612452d7370c90f2c499848cd177e6
Cristi Prodan <cprodan@ptc.com>
2018-03-28 13:55:16 +0300

    Able to upload file

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,37 @@
 package controllers
 
+import java.nio.file.Paths
 import javax.inject._
+
 import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+
+  def importFromCsv() =  Action(parse.multipartFormData) { implicit request =>
+    request.body.file("csv_file").map { csvFile =>
+      val filename = Paths.get(csvFile.filename).getFileName
+      // csvFile.ref.moveTo(Paths.get(s"/tmp/$filename"), replace = true) // possible race condition
+
+      // csvFile.ref.getAbsolutePath
+
+      val source = scala.io.Source.fromFile(csvFile.ref.getAbsolutePath)
+      val lines = try source.mkString finally source.close()
+
+      play.Logger.info("Lines: "+ lines)
+
+      Redirect(routes.HomeController.index()).flashing(
+        "success" -> "File has been uploaded")
+    }.getOrElse {
+      Redirect(routes.HomeController.index()).flashing(
+        "error" -> "Missing file")
+    }
   }
 }
---
+++ app/models/Transaction.scala
@@ -0,0 +1,18 @@
+package models
+
+case class Transaction() {
+
+}
+
+
+object Transaction {
+
+  def insert() {
+
+  }
+
+  def getAll(symbol: String): List[Transaction] = {
+    List("")
+  }
+
+}
