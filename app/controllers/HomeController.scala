7ba75865cb491f808be1acde9381245a85278cbe
Cristi Prodan <cprodan@ptc.com>
2018-07-18 09:08:30 +0300

    A bit of cleanup - menu is in a good shape

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -19,26 +19,4 @@ class HomeController @Inject()(cc: ControllerComponents,
     Ok(views.html.index())
   }
 
-
-  def importFromCsv() =  Action(parse.multipartFormData) { implicit request =>
-    request.body.file("csv_file").map { csvFile =>
-      val filename = Paths.get(csvFile.filename).getFileName
-      // csvFile.ref.moveTo(Paths.get(s"/tmp/$filename"), replace = true) // TODO possible race condition
-
-      val source = scala.io.Source.fromFile(csvFile.ref.getAbsolutePath)
-      val data = try source.mkString finally source.close()
-
-      val lines = data.split("\n")
-      play.Logger.info("Lines: "+ lines.toString)
-      //lines.takeRight(lines.length - 1).foreach(line => GoogleParser.parse(line))
-
-      GoogleParser.parse(data)(database)
-
-      Redirect(routes.HomeController.index()).flashing(
-        "success" -> "File has been uploaded")
-    }.getOrElse {
-      Redirect(routes.HomeController.index()).flashing(
-        "error" -> "Missing file")
-    }
-  }
 }
--- app/controllers/TransactionsController.scala
+++ app/controllers/TransactionsController.scala
@@ -1,8 +1,9 @@
 package controllers
 
+import java.nio.file.Paths
 import javax.inject.{Inject, Singleton}
 
-import models.Transaction
+import models.{GoogleParser, Transaction}
 import modelviews.TrxListMV
 import play.api.db.Database
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
@@ -16,6 +17,32 @@ class TransactionsController @Inject()(cc: ControllerComponents,
       TrxListMV.getAll()
     }
     Ok(views.html.transactions(transactions))
+  }
+
+  def importTx() = Action { implicit request =>
+    Ok(views.html.importTx())
+  }
+
+  def importFromCsv() =  Action(parse.multipartFormData) { implicit request =>
+    request.body.file("csv_file").map { csvFile =>
+      val filename = Paths.get(csvFile.filename).getFileName
+      // csvFile.ref.moveTo(Paths.get(s"/tmp/$filename"), replace = true) // TODO possible race condition
+
+      val source = scala.io.Source.fromFile(csvFile.ref.getAbsolutePath)
+      val data = try source.mkString finally source.close()
+
+      val lines = data.split("\n")
+      play.Logger.info("Lines: "+ lines.toString)
+      //lines.takeRight(lines.length - 1).foreach(line => GoogleParser.parse(line))
+
+      GoogleParser.parse(data)(database)
+
+      Redirect(routes.HomeController.index()).flashing(
+        "success" -> "File has been uploaded")
+    }.getOrElse {
+      Redirect(routes.HomeController.index()).flashing(
+        "error" -> "Missing file")
+    }
   }
 
 }
